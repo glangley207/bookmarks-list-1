f27a64fbdb9aa2b67376f6520c39442cd5956893
Glitch (doc-links-1) <none>
2019-03-16 20:57:57 +0000

    🍕🙍 Updated with Glitch

--- script.js
+++ script.js
@@ -15,14 +15,26 @@ function fix_left_margin(s, factor){
           return s}}
 // ---------------------------------------------------
 function morph_list(id, indent_factor=6) {
-    let make_anchor = (s,m)=>{
-           return s.replace(re_url,`<a href="${m[0]}">${m[0]}</a>`)}
-    let sigma         = []                    //  collect
-    let $list         = $(`#${id}`)
-    let lines         = $list.html().split('\n')
-    lines.forEach((s)=>{
-        let m    = re_url.exec(s)               // line is url
+  let $list         = $(`#${id}`)
+  let lines         = $list.html().split('\n')
+  install_list(id, lines, indent_factor)}
+
+// ---------------------------------------------------
+function fetch_install_list(id, path, indent_factor=6){
+      fetch(path)
+        .then(res=>res.text())
+        .then(dat=>{
+            let lines         = dat.split('\n')
+            install_list(id, lines, indent_factor)})
+        .catch((err)=>{
+            console.log(`ERR: ${err}`)})}
+
+// ---------------------------------------------------
+function install_list(id, list, indent_factor){
+    let make_anchor = (s,m)=>s.replace(re_url,`<a href="${m[0]}">${m[0]}</a>`)
+    let sigma       = []                    //  collect
+    list.forEach((s)=>{
+        let m    = re_url.exec(s)          // line is url
         let s_   = m ? make_anchor(s,m)  :  s;
         sigma.push(fix_left_margin(s_, indent_factor))})
     $(`#${id}`).html(sigma.join('\n'))}
-
