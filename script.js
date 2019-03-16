56cf94f49581fb1fc41de0f3b436e905d47a2381
Glitch (doc-links-1) <none>
2019-03-16 18:26:23 +0000

    🚥🙇 Updated with Glitch

--- script.js
+++ script.js
@@ -6,13 +6,22 @@
 // let re_url            = /\b(https?:\/\/.*?\.[a-z]{2,4}\b)/;
 let re_url            = /http[s]:\/\/[^ ]+/g    // cheep trick
 
-    function morph_list(id) {
-        // convert url into links
-        let r             = $(`#${id}`)
-        let lines         = r.text().split('\n')
-        let sig           = []   // sigma / collect
-        lines.forEach((v,i)=>{
-            let m    = re_url.exec(v)
-            if(m)    {v = v.replace(re_url,`<a href="${m[0]}">${m[0]}</a>`)}
-            sig.push(v)})
-        $(`#${id}`).html(sig.join('\n'))}
+function fix_left_margin(v){
+        let re            = /( *)(.*)/
+        let m    = re.exec(v)
+        if (m) {
+          let sz         = m[1].length * 5
+          return `<div style="margin-left:${sz}px;">${m[2]}</div>`
+        } else {
+          return v}}
+
+function morph_list_1(id) {
+// convert url into links
+let r             = $(`#${id}`)
+let lines         = r.text().split('\n')
+let sig           = []   // sigma / collect
+lines.forEach((v,i)=>{
+    let m    = re_url.exec(v)
+    if(m)    {v = v.replace(re_url,`<a href="${m[0]}">${m[0]}</a>`)}
+    sig.push(v)})
+$(`#${id}`).html(sig.join('\n'))}
